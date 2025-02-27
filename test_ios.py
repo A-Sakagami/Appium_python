adffa014650f862e7eb53949acf95270d7205766
Akiyasu Sakagami <sakagami@genz.jp>
2025-02-27 17:13:46 +0900

    XCUI TestDriverを利用するように変更

--- test_ios.py
+++ test_ios.py
@@ -1,14 +1,13 @@
 from appium.webdriver.appium_service import AppiumService
-# from appium.options.ios import XCUITestOptions
-from appium.options.common import AppiumOptions  # 追加
+from appium.options.ios import XCUITestOptions
 # remote_server_addr の設定方法が変更され、新しい ClientConfig を使用するよう推奨された 2025/2/18
 from selenium.webdriver.remote.remote_connection import RemoteConnection
 from appium import webdriver
 import pytest
 
-@pytest.fixture
+@pytest.fixture(scope="module")
 def ios_driver():
-    options = AppiumOptions()
+    options = XCUITestOptions()
     options.set_capability("platformName", "iOS")
     # options.set_capability("platformVersion", "16.0")
     options.set_capability("platformVersion", "18.3")
@@ -18,10 +17,7 @@ def ios_driver():
     options.set_capability("bundleId", "com.apple.Preferences")
     options.set_capability("noReset", True)
 
-    # `RemoteConnection` を使用
-    command_executor = RemoteConnection("http://127.0.0.1:4723")
-
-    driver = webdriver.Remote(command_executor=command_executor, options=options)
+    driver = webdriver.Remote("http://localhost:4723", options=options)
 
     yield driver
     driver.quit()
@@ -30,3 +26,6 @@ def test_ios_launch(ios_driver):
     assert ios_driver.is_app_installed("com.apple.Preferences")
     ios_driver.terminate_app("com.apple.Preferences")
     assert ios_driver.query_app_state("com.apple.Preferences") == 1
+
+if __name__ == "__main__":
+    pytest.main(["-v", "-s", __file__])
